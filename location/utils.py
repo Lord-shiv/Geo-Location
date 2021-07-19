267e414a60684eccc1b79430ff6943a9db84485b
shivam <shivamchouhan728@gmail.com>
2021-07-19 13:35:41 +0530

    👷 ~ Map and lcoation

--- location/utils.py
+++ location/utils.py
@@ -9,3 +9,20 @@ def get_geo(ip):
     city = g.city(ip)
     lat, lon = g.lat_lon(ip)
     return country, city, lat, lon
+
+
+def get_center_coordinates(latA, longA, latB=None, longB=None):
+    cord = (latA, longA)
+
+    if latB:
+        cord = [(latA + latB)/2, (longA+longB)/2]
+    return cord
+
+
+def get_zoom(distance):
+    if distance <= 100:
+        return 11
+    elif distance > 100 and distance <= 500:
+        return 8
+    else:
+        return 2
--- location/views.py
+++ location/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import get_object_or_404, render
 # locals
 from . models import LocationMeasurement
 from . forms import LocationMeasurementForm
-from . utils import get_geo
+from . utils import get_geo, get_center_coordinates, get_zoom
 # geopy
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
+# folium
+import folium
 
 
 def calculate_distance_view(request):
@@ -14,38 +16,56 @@ def calculate_distance_view(request):
 
     # GEOPY
     geolocator = Nominatim(user_agent="location")
-    ip = '209.95.50.14'
+    ip = '72.46.131.10'
     country, city, lat, lon = get_geo(ip)
     location = geolocator.geocode(city)
 
-    # location coordinates
+    # Location coordinates
     l_lat = lat
     l_lon = lon
     pointA = (l_lat, l_lon)
 
-    # initial folium map
+    # Initial folium map
+    m = folium.Map(width=1080, height=600,
+                   location=get_center_coordinates(l_lat, l_lon), zoom_start=6)
+    # location marker
+    folium.Marker([l_lat, l_lon], popup=city['city'],
+                  icon=folium.Icon(color='red')).add_to(m)
 
     if form.is_valid():
         instance = form.save(commit=False)
-        d_destination = form.cleaned_data.get('destination')
-        g_destination = geolocator.geocode(d_destination)
+        destination_ = form.cleaned_data.get('defined_destination')
+        destination = geolocator.geocode(destination_)
 
-        # destination coordinates
-        d_lat = g_destination.latitude
-        d_lon = g_destination.longitude
+        # Destination coordinates
+        d_lat = destination.latitude
+        d_lon = destination.longitude
         pointB = (d_lat, d_lon)
 
-        # distance calculation
+        # Distance calculation
         distance = round(geodesic(pointA, pointB).km, 2)
 
         # folium map modification
+        m = folium.Map(width=1080, height=640, location=get_center_coordinates(
+            l_lat, l_lon, d_lat, d_lon), zoom_start=get_zoom(distance))
+        # location marker
+        folium.Marker([l_lat, l_lon], tooltip='click me', popup=city['city'],
+                      icon=folium.Icon(color='red')).add_to(m)
+
+        # destination marker
+        folium.Marker([d_lat, d_lon], tooltip='click me', popup=destination,
+                      ).add_to(m)
+
         instance.user_location = location
         instance.distance = distance
         instance.save()
 
+    m = m._repr_html_()
+
     context = {
         'distance': obj,
         'form': form,
+        'map': m
     }
 
-    return render(request, 'location/test.html', context)
+    return render(request, 'location/main.html', context)
